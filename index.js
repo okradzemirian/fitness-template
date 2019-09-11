2df18fe1af2a7129949b807d5461f77cfdc1cdf0
okradzemirian <okradzemirian@gmail.com>
2019-09-11 22:46:43 +0400

    Added navbar background on scroll

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const $navToggleButton = document.getElementById('nav-toggle-button')
 const $navList = document.getElementById('nav-toggle')
 const $navItems = document.querySelectorAll('.nav__link')
 
+// nav toggle on mobile
+
 const toggleNavOpen = () => {
     $navList.classList.toggle('nav__list--open')
     document
@@ -19,3 +21,25 @@ $navToggleButton.addEventListener('click', () => {
 $navItems.forEach($navItem => {
     $navItem.addEventListener('click', toggleNavOpen)
 })
+
+// change nav background on scroll
+
+const $header = document.querySelector('.header')
+const $homeSection = document.getElementById('home')
+
+const aboutSectionObserver = new IntersectionObserver(
+    (entries, observer) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                $header.classList.add('header-scrolled')
+            } else {
+                $header.classList.remove('header-scrolled')
+            }
+        })
+    },
+    {
+        rootMargin: '-200px 0px 0px 0px',
+    },
+)
+
+aboutSectionObserver.observe($homeSection)
